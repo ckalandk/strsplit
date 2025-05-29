8ecd5f35308701fe93346774eb2778c17ab3f5ca
Khaled Kessoum <ckalandk.main@gmail.com>
2025-05-30 00:19:51 +0100

    bump version to 1.0.1

--- src/strsplitter/__init__.py
+++ src/strsplitter/__init__.py
@@ -2,6 +2,6 @@ from .strsplitter import split
 from .splitter import Splitter, SplitByStr, SplitByLength, SplitByAnyChar
 
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 __all__ = ['split', 'Splitter', 'SplitByStr', 'SplitByLength', 'SplitByAnyChar']
