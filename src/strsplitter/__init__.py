d6d3b45efbaff75d94c77a4ae9f78c032d4ed4e3
Khaled Kessoum <ckalandk.main@gmail.com>
2025-05-31 00:27:12 +0100

    Add docstrings to improve code documentation

--- src/strsplitter/__init__.py
+++ src/strsplitter/__init__.py
@@ -1,6 +1,5 @@
+from .splitter import SplitByAnyChar, SplitByLength, SplitByStr, Splitter
 from .strsplitter import split
-from .splitter import Splitter, SplitByStr, SplitByLength, SplitByAnyChar
-
 
 __version__ = '1.0.1'
 
--- src/strsplitter/__main__.py
+++ src/strsplitter/__main__.py
@@ -1,31 +1,34 @@
-from strsplitter.splitter import SplitByAnyChar
-from .strsplitter import split
-import click
 import sys
+
+import click
+
+from strsplitter.splitter import SplitByAnyChar
+
+from .strsplitter import split
 
 
 @click.command()
 @click.option(
-	'--delimiters',
-	'-d',
-	default=' \n\t\r\f',
-	help='Delimiters to use for splitting the input string.',
+    '--delimiters',
+    '-d',
+    default=' \n\t\r\f',
+    help='Delimiters to use for splitting the input string.',
 )
 @click.argument('input_file', nargs=1)
 def main(delimiters: str, input_file: str):
-	"""Splits the content of the input file by the specified delimiters."""
-	try:
-		with open(input_file, 'r', encoding='utf-8') as file:
-			content = file.read()
-	except FileNotFoundError:
-		print(f"Error: File '{input_file}' not found.", file=sys.stderr)
-		sys.exit(1)
-	_delimiters = bytes(delimiters, 'utf-8').decode('unicode_escape')
-	result = split(content, SplitByAnyChar(_delimiters))
+    """Splits the content of the input file by the specified delimiters."""
+    try:
+        with open(input_file, encoding='utf-8') as file:
+            content = file.read()
+    except FileNotFoundError:
+        print(f"Error: File '{input_file}' not found.", file=sys.stderr)
+        sys.exit(1)
+    _delimiters = bytes(delimiters, 'utf-8').decode('unicode_escape')
+    result = split(content, SplitByAnyChar(_delimiters))
 
-	for part in result:
-		click.echo(part)
+    for part in result:
+        click.echo(part)
 
 
 if __name__ == '__main__':
-	main()
+    main()
